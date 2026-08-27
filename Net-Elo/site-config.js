fde218a56ef51bf407a9af00b5842e18c68600dd
InSight Local <insight.local@icloud.com>
2026-08-27 19:59:21 +0200

    Net Elo : configuration analytics prête

---
+++ Net-Elo/site-config.js
@@ -0,0 +1,5 @@
+window.NET_ELO_CONFIG = {
+  // Renseigner l'identifiant GA4 sous la forme G-XXXXXXXXXX pour activer Google Analytics.
+  // Tant que ce champ est vide, aucun script Google Analytics n'est chargé.
+  gaMeasurementId: ""
+};
